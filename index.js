a45fe8c9b33ed581cb4cc9b11a5d2ead2304dbf4
PirateCoderz <118294175+PirateCoderz@users.noreply.github.com>
2024-11-14 19:23:25 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+//  var let const 
+
+// scope of var is global
+
+// scope of let and const is local/block level
+
+
+// variables initialized by var and let are changeable/mutable
+
+// but initialized by const are fixed/constant
+
+// variable names are case sensitive
+
+
+const name = 5;
+
+
+
+
+let x = 10;
+
+x  = "Ahmad";
+const PI = 3.1425;
+
+// const pi = 3.14;
+
+// x = 15;
+
+/*
+let a = 10;
+let b = 15;
+let s = a + b;
+*/
+
+// console.log(PI);
+
+// console.log(pi);
+console.log(x);
+
+
+
